a9cf4b1b389704a534c2695447b607f62681ac43
yurochka <yurychernyak9@gmail.com>
2018-11-02 18:26:44 +0200

    added proptypes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,15 @@ class Footer extends Component{
     return (
       <div className="footer">
         <h2>Look at our dogs</h2><br/>
+        <form>
+          <input type="text" /><br/>
+          <button>Find more</button>
+        </form>
+        {
+          this.props.imgs.map((item,i)=>
+          <img src={item} heigth="250" width="250"/>
+          )
+        }
       </div>
     )
   }
--- src/components/My_images.js
+++ src/components/My_images.js
@@ -6,39 +6,6 @@ import ContactForm from './ContactForm'
 import MyImagesWithoutForm from './MyImagesWithoutForm'
 class My_images extends Component{
 
-/*
-    constructor(props) {
-      super(props);
-      this.state = {
-        breeds: [],
-        downloading: false
-      };
-    }
-
-    componentDidMount() {
-      this.handleFetch(3);
-    }
-
-
-  handleFetch = (param) => {
-    //console.log('---',param);
-    this.setState({
-      downloading: true
-    })
-    let currentBreeds = this.state.breeds;
-    Apiclient.get(`https://dog.ceo/api/breeds/image/random/${param}`)
-      .then((data) => {
-
-        currentBreeds = currentBreeds.concat(data.message);
-
-        //console.log('CURENTBREDS', currentBreeds);
-        this.setState({breeds: currentBreeds, downloading: false})
-      });
-  }
-
-*/
-
-
   render(){
     return (
       <div className="formAndImages">
